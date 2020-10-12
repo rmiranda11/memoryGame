437aa734a506e7966f321ed6170fbaa43b2c09e4
rmiranda11 <r_miranda_11@yahoo.com>
2020-10-12 09:01:53 -0500

    responsive update

--- memory/src/cardData.js
+++ memory/src/cardData.js
@@ -83,135 +83,135 @@ const cardData = [
 
     },
     
-    // {
-    //     key:8,
-    //     animal:"sombrero",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"https://sep.yimg.com/ay/skyimage/m104-sombrero-galaxy-in-infrared-9.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+    {
+        key:8,
+        animal:"sombrero",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"https://sep.yimg.com/ay/skyimage/m104-sombrero-galaxy-in-infrared-9.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
 
-    // },
-    // {
-    //     key:9,
-    //     animal:"sombrero",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"https://sep.yimg.com/ay/skyimage/m104-sombrero-galaxy-in-infrared-9.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+    },
+    {
+        key:9,
+        animal:"sombrero",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"https://sep.yimg.com/ay/skyimage/m104-sombrero-galaxy-in-infrared-9.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
     
-    // },
-    // {
-    //     key:10,
-    //     animal:"fireworks",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"https://sep.yimg.com/ay/skyimage/fireworks-galaxy-9.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+    },
+    {
+        key:10,
+        animal:"fireworks",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"https://sep.yimg.com/ay/skyimage/fireworks-galaxy-9.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
 
-    // },
-    // {
-    //     key:11,
-    //     animal:"fireworks",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"https://sep.yimg.com/ay/skyimage/fireworks-galaxy-9.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
-
- 
-    // },
-    // {
-    //     key:12,
-    //     animal:"big-bear",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"https://sep.yimg.com/ay/skyimage/big-bear-spiral-galaxy-9.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+    },
+    {
+        key:11,
+        animal:"fireworks",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"https://sep.yimg.com/ay/skyimage/fireworks-galaxy-9.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
 
  
-    // },
-    // {
-    //     key:13,
-    //     animal:"big-bear",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"https://sep.yimg.com/ay/skyimage/big-bear-spiral-galaxy-9.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+    },
+    {
+        key:12,
+        animal:"big-bear",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"https://sep.yimg.com/ay/skyimage/big-bear-spiral-galaxy-9.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
 
  
-    // },
-    // {
-    //     key:14,
-    //     animal:"cigar",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"http://www.godandscience.org/images/universe/m82ngc3034cigargalaxy.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+    },
+    {
+        key:13,
+        animal:"big-bear",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"https://sep.yimg.com/ay/skyimage/big-bear-spiral-galaxy-9.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
 
  
-    // },
-    // {
-    //     key:15,
-    //     animal:"cigar",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"http://www.godandscience.org/images/universe/m82ngc3034cigargalaxy.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+    },
+    {
+        key:14,
+        animal:"cigar",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"http://www.godandscience.org/images/universe/m82ngc3034cigargalaxy.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
 
  
-    // },
-    // {
-    //     key:16,
-    //     animal:"pinwheel",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"http://www.godandscience.org/images/universe/southernpinwheelgalaxym83.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+    },
+    {
+        key:15,
+        animal:"cigar",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"http://www.godandscience.org/images/universe/m82ngc3034cigargalaxy.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
 
  
-    // },
-    // {
-    //     key:17,
-    //     animal:"pinwheel",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"http://www.godandscience.org/images/universe/southernpinwheelgalaxym83.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+    },
+    {
+        key:16,
+        animal:"pinwheel",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"http://www.godandscience.org/images/universe/southernpinwheelgalaxym83.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
 
  
-    // },
-    // {
-    //     key:18,
-    //     animal:"milkyway",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"http://www.godandscience.org/images/universe/milkywaygalaxy.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+    },
+    {
+        key:17,
+        animal:"pinwheel",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"http://www.godandscience.org/images/universe/southernpinwheelgalaxym83.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
 
  
-    // },
-    // {
-    //     key:19,
-    //     animal:"milkyway",
-    //     isFlipped:false,
-    //     isMatched:false,
-    //     picHere: "",
-    //     pic:"http://www.godandscience.org/images/universe/milkywaygalaxy.jpg",
-    //     pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+    },
+    {
+        key:18,
+        animal:"milkyway",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"http://www.godandscience.org/images/universe/milkywaygalaxy.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
 
  
-    // }
+    },
+    {
+        key:19,
+        animal:"milkyway",
+        isFlipped:false,
+        isMatched:false,
+        picHere: "",
+        pic:"http://www.godandscience.org/images/universe/milkywaygalaxy.jpg",
+        pic2: "https://i.ibb.co/3R5Cs3B/X.jpg"
+
+ 
+    }
 
 
 ]
